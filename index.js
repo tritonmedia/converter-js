48f8339fc780e1636677e591b367e320b53e692f
Jared Allard <jaredallard@outlook.com>
2018-08-18 15:44:01 -0700

    chore: add eslintrc

--- index.js
+++ index.js
@@ -6,20 +6,19 @@
  * @version 1
  */
 
-const debug  = require('debug')('media:converter')
+const debug = require('debug')('media:converter')
 const Config = require('triton-core/config')
-const dyn    = require('triton-core/dynamics')
-const kue    = require('kue')
-const queue  = kue.createQueue({
+const dyn = require('triton-core/dynamics')
+const kue = require('kue')
+const queue = kue.createQueue({
   redis: dyn('redis')
 })
-
 
 debug.log = console.log.bind(console)
 
 debug('init', Date.now())
 
-const init   = async () => {
+const init = async () => {
   const config = await Config('converter')
 
   debug('eval-constraints')
@@ -33,8 +32,8 @@ const cleanup = (code = 0) => {
   debug('cleanup')
   queue.shutdown(1000, err => {
     debug('kue:shutdown', err)
-    process.exit(code);
-  });
+    process.exit(code)
+  })
 }
 
 process.on('SIGINT', () => {
@@ -46,4 +45,4 @@ process.on('unhandledRejection', error => {
   debug('Uncaught Execption:', error)
 
   cleanup(1)
-});
+})
